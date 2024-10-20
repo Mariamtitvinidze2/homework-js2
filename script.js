6bf2b318fa53cc7774e243c178401c891d30099a
mariam titvinidze <titvinidzemariam3@gmail.com>
2024-10-20 13:09:38 +0400

    js2

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+let num = 18;
+let square = num * num;
+
+if (square > 100) {
+  console.log("მეტია ასზე");
+} else {
+  console.log("ნაკლებია ასზე");
+}
+
+ 
+let number = 75; 
+if (number > 50 && number < 100) {
+  console.log("რიცხვი 50-ზე მეტია და 100-ზე ნაკლები");
+} else {
+  console.log("რიცხვი არ არის 50-ზე მეტი ან 100-ზე ნაკლები");
+}
+
+
+
+let ricxvi = -29; 
+if (number <= 0) {
+  console.log("რიცხვი ნულის ტოლია ან უარყოფითი");
+} else {
+  console.log("რიცხვი დადებითია");
+}
+
+
+ 
+let length = 45; 
+let width = 38;
+let perimeter = 2 * (length + width);
+
+if (perimeter > 50) {
+  console.log("პერიმეტრი მეტია 50-ზე");
+} else {
+  console.log("პერიმეტრი ნაკლებია 50-ზე");
+}
